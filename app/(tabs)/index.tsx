a75cbf10a985e6c86423e2ee1f9ed5660f05a333
adam <afhodges@pm.me>
2026-04-05 11:57:46 +0200

    Add weather API integration

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,82 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const loadingMessage = "Loading weather data...";
+  const errorMessage = "Error loading weather data";
+
+  /**
+   * Fetch weather data from the API when the component mounts. The API requires a User-Agent header, so we include that in the request. We handle loading, error, and success states accordingly.
+   */
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=66.0&lon=20.9",
+          {
+            headers: {
+              "User-Agent": "norrsken/1.0 adamhodges@live.co.uk",
+            },
+          },
+        );
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(String(error));
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchWeatherData();
+  }, []);
+
+  if (loading) return <Text style={styles.text}>{loadingMessage}</Text>;
+  if (error) return <Text style={styles.text}>{errorMessage}</Text>;
+  if (!data) return <Text style={styles.text}>No data available</Text>;
+
+  const current = data.properties.timeseries[0].data.instant.details;
+  const temperature = current.air_temperature;
+  const precipiration_rate = current.precipitation_rate;
+  const relative_humidity = current.relative_humidity;
+  const wind_from_direction = current.wind_from_direction;
+  const wind_speed = current.wind_speed;
+  const wind_speed_of_gust = current.wind_speed_of_gust;
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Weather Data:</Text>
+      <Text style={styles.text}>Temperature: {temperature}°C</Text>
+      <Text style={styles.text}>
+        Precipitation Rate: {precipiration_rate} mm/h
+      </Text>
+      <Text style={styles.text}>Relative Humidity: {relative_humidity}%</Text>
+      <Text style={styles.text}>
+        Wind From Direction: {wind_from_direction}°
+      </Text>
+      <Text style={styles.text}>Wind Speed: {wind_speed} m/s</Text>
+      <Text style={styles.text}>
+        Wind Speed of Gust: {wind_speed_of_gust} m/s
+      </Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 50,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    justifyContent: "center",
+    paddingTop: 50,
+    paddingLeft: 20,
   },
 });
