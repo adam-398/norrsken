928716fb8ee0b8a42c0e2e4dcb8973cded77c442
adam <afhodges@pm.me>
2026-04-05 12:56:16 +0200

    Added location functionality, could not get locaiton working with grapheneOS so added fallback coordinates

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
@@ -5,18 +6,42 @@ export default function HomeScreen() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null,
+  );
 
   const loadingMessage = "Loading weather data...";
   const errorMessage = "Error loading weather data";
 
-  /**
-   * Fetch weather data from the API when the component mounts. The API requires a User-Agent header, so we include that in the request. We handle loading, error, and success states accordingly.
-   */
   useEffect(() => {
-    const fetchWeatherData = async () => {
+    /**
+     * Fetch weather data from the API when the component mounts.
+     *  The API requires a User-Agent header, so we include that in the request.
+     *  We handle loading, error, and success states accordingly.
+     */
+    const fetchWeatherandLocation = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("Permission to access location was denied");
+        setLoading(false);
+        return;
+      }
+
+      let lat = 66.0;
+      let lon = 13.0;
+
+      try {
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        lat = location.coords.latitude;
+        lon = location.coords.longitude;
+      } catch (e) {
+        console.log("location error, using backup coordinates:");
+      }
+
       try {
         const response = await fetch(
-          "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=66.0&lon=20.9",
+          `https://api.met.no/weatherapi/nowcast/2.0/complete?lat=${lat}&lon=${lon}`,
           {
             headers: {
               "User-Agent": "norrsken/1.0 adamhodges@live.co.uk",
@@ -26,18 +51,25 @@ export default function HomeScreen() {
         const json = await response.json();
         setData(json);
       } catch (error) {
+        console.log("weather fetch error:", error);
         setError(String(error));
       } finally {
         setLoading(false);
       }
     };
-    fetchWeatherData();
+
+    fetchWeatherandLocation();
   }, []);
 
   if (loading) return <Text style={styles.text}>{loadingMessage}</Text>;
   if (error) return <Text style={styles.text}>{errorMessage}</Text>;
   if (!data) return <Text style={styles.text}>No data available</Text>;
 
+  /**
+   * Extract the relevant weather data from the API response.
+   *  The API returns a timeseries array, and we take the first entry to get the current weather conditions.
+   *  We then extract specific details such as temperature, precipitation rate, humidity, wind direction, and wind speed to display in the UI.
+   */
   const current = data.properties.timeseries[0].data.instant.details;
   const temperature = current.air_temperature;
   const precipiration_rate = current.precipitation_rate;
